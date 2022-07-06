f14f1ca9dfc53a3f1a217228445f3ab94f7e26e8
june65 <june0100726@gmail.com>
2022-07-06 11:19:31 +0900

    Issue(#1) Add project1-1

---
+++ object_detection/project1-1/project1-1.py
@@ -0,0 +1,13 @@
+
+import matplotlib.pyplot as plt
+import cv2
+
+cv2_image = cv2.imread('beatles01.png')
+cv2.imwrite('beatles02_cv.png', cv2_image)
+print('cv_image type:', type(cv2_image), ' cv_image shape:', cv2_image.shape)
+
+plt.figure(figsize=(10, 10))
+img = plt.imread('beatles02_cv.png')
+plt.imshow(img)
+plt.show()
+s
